d89996b54186f65f7e78aa9bc0a0fc978fc447e0
hey-dola <dolamanirohidas696@gmail.com>
2024-06-09 20:30:06 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,5 +13,17 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routers import
+import userRouter from './routes/user.routes.js'
+
+
+
+// router declaration
+app.use("/api/v1/users", userRouter)    //http://localhost:8000/api/v1/users/register
+
+
+
+
+
 
 export { app }
